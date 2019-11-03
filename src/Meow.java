a4c9e13298ab17fb3bfdc87abdbe753694620b17
Putin200 <aaaadancc@gmail.com>
2019-11-03 13:48:56 +0500

    first commit

---
+++ src/Meow.java
@@ -0,0 +1,17 @@
+public class Meow {
+    public static void main(String[] args) {
+        Woof gaf = new Woof();
+        gaf.all_data("Бима","15","Коричневая","Такса");
+        System.out.println("Кличка собаки - " + gaf.getName() + " Вес - " + gaf.getWeight() + "кг" + " Окраска - " + gaf.getColor() + " Порода - " + gaf.getPoroda());
+        Woof mur = new Woof();
+        mur.all_data("Бума","25","Белая с коричневыми пятнами","Корги");
+        System.out.println(mur.getAll_data());
+        Cat catyara  = new Cat();
+        catyara.all1_data("собака","5","Полосатая","дворовая");
+        System.out.println(catyara.getAll1_data());
+
+
+
+
+    }
+}
